63614041b496c25924ae5a8887e7dc2be19590af
mujtabaalhabib <mujtaba.alhabib@gmail.com>
2016-03-24 16:17:45 -0400

    Added files via upload

---
+++ php-Muj&Nnam.php
@@ -0,0 +1,16 @@
+<h1>About Us</h1>
+<p><B>  This is a project for NET4901. We are a group of 4th year Bachelor of Information Technology students majoring in Network Technology.</p>
+<p>  We decided to divide the work to two different teams which are the backend development team and web development team.</p>
+<p> The backend development team consists of : <ul>
+                                               <li>John Mitchell</li>
+                                               <li>Syed Taqi Raza</li>
+                                               <li>Alex Sol</li>
+                                               <li>Logan McDonald </li>
+                                              </ul>
+The web development team consists of : <ul>
+                                        <li>Nnamdi Azubike</li>
+                                        <li>Mujtaba Alhabib</li>
+                                        <li>Ahmad Nasrallah</li>
+                                       </ul>
+</B>
+</p>
